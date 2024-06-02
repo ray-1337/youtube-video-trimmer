c15909a8eee28bb2d352830688fcfe8ac60636ee
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-06-02 08:32:17 +0800

    include Mantine core styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 
 import { MantineProvider } from "@mantine/core";
 
+import "@mantine/core/styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
