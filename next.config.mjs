72e25771218422a09ef8ac9d15155285012b591f
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-06-18 22:57:32 +0800

    add CDN url on production

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  assetPrefix: process.env.NODE_ENV !== "development" ? "https://ytfs-static-assets.b-cdn.net/" : undefined
 };
 
 export default nextConfig;
