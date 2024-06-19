d2ac1a8687459eb20db53c86bb9f4a55f7dedb1d
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-06-19 17:38:14 +0800

    add project config

---
+++ src/components/config/ProjectConfig.ts
@@ -0,0 +1,9 @@
+import ms from "ms";
+
+export default {
+  // how many times user can trim videos at the range of time
+  userRetries: 4,
+
+  // range of time; lets say 2 minutes, then users can only trim like 3 clips in between 2 minutes, otherwise they have to wait for the next 2 minutes
+  sessionExpiration: ms("2m")
+};
